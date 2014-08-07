c3b6aaab8d834bfecbff71169bb2833bf46f4143
Steven <Steven@Maydigital.com>
2014-08-06 17:46:27 -0700

    Update admin.py

--- admin.py
+++ admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-
+from django.contrib.auth.models import User
 from Merkhet.models import HourEntry
 class HourEntryAdmin(admin.ModelAdmin):
-    list_display = ('project','name','task','notes','pub_date','hours', 'cost','billable','billed')
+    list_display = ('name','project','task','notes','pub_date','hours', 'cost','billable','billed')
 
 admin.site.register(HourEntry, HourEntryAdmin)
 
