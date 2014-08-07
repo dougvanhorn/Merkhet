13d8fc33c1f3e6b32929f42308766ac319756da4
Steven <Steven@Maydigital.com>
2014-08-06 17:45:45 -0700

    Update models.py

--- models.py
+++ models.py
@@ -1,20 +1,7 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 
-class HourEntry(models.Model):
-    
-    name = models.CharField(max_length=30)
-    project = models.CharField(max_length=40)
-    task = models.CharField(max_length=30)
-    notes = models.TextField(max_length=4)
-    pub_date = models.DateField('Date Worked')
-    hours = models.DecimalField(max_digits=5, decimal_places=2)
-    cost = models.IntegerField(default=0)
-    billable = models.BooleanField(default=False)
-    billed = models.BooleanField(default=False)
-    class Meta:
-    	verbose_name_plural = "Hour Entries"
-       
 class Project(models.Model):
     name = models.CharField(max_length=60)
     def __unicode__(self):  
@@ -28,3 +15,21 @@ class Task(models.Model):
         return self.name
     class Meta:
 	verbose_name_plural = "Tasks"
+
+class HourEntry(models.Model):
+    
+    name = models.ForeignKey(User)
+    project = models.ForeignKey(Project)
+    task = models.ForeignKey(Task)
+    notes = models.TextField(max_length=4)
+    pub_date = models.DateField('Date Worked')
+    hours = models.DecimalField(max_digits=5, decimal_places=2)
+    cost = models.IntegerField(default=0)
+    billable = models.BooleanField(default=False)
+    billed = models.BooleanField(default=False)
+    class Meta:
+    	verbose_name_plural = "Hour Entries"
+       
+
+
+
